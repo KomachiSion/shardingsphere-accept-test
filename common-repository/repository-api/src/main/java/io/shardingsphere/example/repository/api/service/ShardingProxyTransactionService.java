67cdc0fbd2f4876ec90e08caeffc1650703295a7
KomachiSion <263976490@qq.com>
2019-03-15 14:19:33 +0800

    Fix compile error

--- common-repository/repository-api/src/main/java/io/shardingsphere/example/repository/api/service/ShardingProxyTransactionService.java
+++ common-repository/repository-api/src/main/java/io/shardingsphere/example/repository/api/service/ShardingProxyTransactionService.java
@@ -18,7 +18,7 @@
 package io.shardingsphere.example.repository.api.service;
 
 import io.shardingsphere.example.repository.api.repository.TransactionTypeRepository;
-import io.shardingsphere.transaction.annotation.ShardingTransactionType;
+import org.apache.shardingsphere.transaction.annotation.ShardingTransactionType;
 import org.apache.shardingsphere.transaction.core.TransactionType;
 import org.springframework.transaction.annotation.Transactional;
 
